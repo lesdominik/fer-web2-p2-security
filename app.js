85ae4af36098f0d08f5e132a78501b0036ff4d91
lesdominik <les.dominik@gmail.com>
2024-10-29 15:50:53 +0100

    Add express built-in middleware

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
 	res.send('Hello there.');
