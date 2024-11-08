1487fdd7ec5d57ba077cb537a46c09deed8b0637
lesdominik <les.dominik@gmail.com>
2024-11-08 01:07:33 +0100

    Create addCommentModal and import bootstrap.js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,6 +2,5 @@ const model = require('../models/commentModel');
 
 exports.getHomePage = (req, res) => {
 	const comments = model.readComments();
-	console.log(comments);
 	res.render('index', { comments });
 };
